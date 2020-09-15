98dbc7bdc4e7371e3e6d57fa640f248a577221e4
Hurkan Dogan <60603139+hurkandogan@users.noreply.github.com>
2020-09-15 22:11:17 +0200

    Connection improvements

--- src/jav12Einsendeaufgaben/frageZwei/db/ConnectionManager.java
+++ src/jav12Einsendeaufgaben/frageZwei/db/ConnectionManager.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class ConnectionManager {
 
-    private static Connection connectionMain = null;
+    private static Connection conn = null;
     private static String mysqlJdbcDriverClass = "org.gjt.mm.mysql.Driver";
     private static String mysqlJdbcConnectionProtocol = "jdbc:mysql://";
     private static String database = "/dbdemo2";
@@ -14,23 +14,30 @@ public class ConnectionManager {
     private static boolean logger = true;
     private static SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
 
-    public static synchronized Connection getConnection(String host, String user, String passwort) throws ClassNotFoundException, SQLException{
-
-        Class.forName(ConnectionManager.mysqlJdbcDriverClass);
-        String mysqlJdbcConnectionUrl = mysqlJdbcConnectionProtocol + host + database;
-        Connection connection = getConnection(mysqlJdbcConnectionUrl, user, passwort);
-        if(logger) {
+    public static synchronized Connection getConnection(String host, String user, String passwort) throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+            String mysqlJdbcConnectionUrl = mysqlJdbcConnectionProtocol + host + database;
+            conn = DriverManager.getConnection(mysqlJdbcConnectionUrl, user, passwort);
             System.out.println("New connection for " + user + " created " + sdf.format(new Date()));
+
+        } catch (Exception ie) {
+            System.out.println("Baglanti kurulamadi " + ie.getMessage());
         }
-        return connection;
+        return conn;
     }
 
-    public static void main(String[] args){
+    /**
+     * TODO: Get Connection iyilestirilmeli
+     * TODO: Close Connection metodu!
+     * @param args
+     */
+    public static void main(String[] args) {
         try {
             Connection conn = ConnectionManager.getConnection("localhost", "demo-user", "");
-        }catch(ClassNotFoundException cnfe){
+        } catch (ClassNotFoundException cnfe) {
             System.out.println(cnfe.getMessage());
-        }catch(SQLException sqle){
+        } catch (SQLException sqle) {
             System.out.println(sqle.getMessage());
         }
     }
