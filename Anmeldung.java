cc49edc518dfc70dc3716f7d417ca35f2eb3b44d
Hurkan Dogan <60603139+hurkandogan@users.noreply.github.com>
2020-09-21 21:42:57 +0200

    Layout Improvements

--- Anmeldung.java
+++ Anmeldung.java
@@ -9,28 +9,35 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import jav12Einsendeaufgaben.angestellterAnmeldung.daten.Angestellter;
 import jav12Einsendeaufgaben.angestellterAnmeldung.daten.Person;
+import jav12Einsendeaufgaben.angestellterAnmeldung.daten.Abteilung;
 
 public class Anmeldung extends JFrame implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
 
     // Fields
     private JLabel jlHeadline, jlFirstname, jlLastname, jlPassword, jlDbUsername;
     private JTextField jtfStatus, jtfFirstname, jtfLastname, jtfDBUsername;
     private JPasswordField jpfPassword;
     private JButton jbLogin, jbExit;
-    private String defaultDBUser = "demo-user";
 
+    // DB Fields
+    private static String defaultDBUser = "demo-user";
     private DbManager dbManager;
 
+    private Person person;
     private Angestellter angestellter;
+    private Abteilung abteilung;
 
     // Constructor
     public Anmeldung(String title) {
         super(title);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.createGUI();
-        this.setSize(600, 400);
+        this.setSize(600, 300);
         this.setLocation(50, 50);
         this.setVisible(true);
+        jtfFirstname.requestFocus();
     }
 
     public static void main(String[] args) {
@@ -40,64 +47,46 @@ public class Anmeldung extends JFrame implements ActionListener {
     public void createGUI() {
         // Labels
         jlHeadline = new JLabel("Personalverwaltung - Zugang nur für Abteilungsleiter", JLabel.CENTER);
-        jlHeadline.setFont(new Font("Serif", Font.BOLD, 25));
+        jlHeadline.setFont(new Font("Serif", Font.BOLD, 20));
         jlHeadline.setForeground(Color.BLUE);
-        jlHeadline.setHorizontalTextPosition(SwingConstants.CENTER);
-        jlFirstname = new JLabel("Vorname");
-        jlLastname = new JLabel("Nachname");
-        jlPassword = new JLabel("Password");
-        jlDbUsername = new JLabel("DB-Benutzername");
+        jlFirstname = new JLabel("Vorname:", JLabel.LEFT);
+        jlLastname = new JLabel("Nachname:", JLabel.LEFT);
+        jlPassword = new JLabel("Password:", JLabel.LEFT);
+        jlDbUsername = new JLabel("DB-Benutzername:", JLabel.RIGHT);
+        this.add(BorderLayout.NORTH, jlHeadline);
         // Text Fields
         jtfFirstname = new JTextField();
         jtfLastname = new JTextField();
         jpfPassword = new JPasswordField();
         jtfDBUsername = new JTextField(defaultDBUser);
+        // Status Text Field
         jtfStatus = new JTextField("Status Field");
         jtfStatus.setFont(new Font("Monospaced", Font.PLAIN, 12));
         jtfStatus.setForeground(Color.WHITE);
         jtfStatus.setBackground(Color.BLACK);
         jtfStatus.setEditable(false);
+        this.add(BorderLayout.SOUTH, jtfStatus);
         // Buttons
         jbLogin = new JButton("Anmelden");
         jbLogin.addActionListener(this);
         jbExit = new JButton("Beenden");
         jbExit.addActionListener(this);
-
         // Content Create
-        JPanel jPanelHeadline = new JPanel(new GridLayout(1, 1, 20, 20));
-        jPanelHeadline.add(jlHeadline);
-        this.add(BorderLayout.NORTH, jPanelHeadline);
-
-        JPanel jPanelMain = new JPanel(new GridLayout(1, 3));
-        jPanelMain.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-        JPanel jPanelLeft = new JPanel(new GridLayout(4, 1, 5, 5));
-        jPanelLeft.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        jPanelLeft.add(jlFirstname);
-        jPanelLeft.add(jtfFirstname);
-        jtfFirstname.setPreferredSize(new Dimension(20, 10));
-        jPanelLeft.add(new JLabel());
-        jPanelLeft.add(new JLabel());
-        jPanelMain.add(jPanelLeft);
-
-        JPanel jPanelCenter = new JPanel(new GridLayout(4, 1, 5, 5));
-        jPanelCenter.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        jPanelCenter.add(jlLastname);
-        jPanelCenter.add(jtfLastname);
-        jPanelCenter.add(jlDbUsername);
-        jPanelCenter.add(jbLogin);
-        jPanelMain.add(jPanelCenter);
-
-        JPanel jPanelRight = new JPanel(new GridLayout(4, 1, 5, 5));
-        jPanelRight.setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 5));
-        jPanelRight.add(jlPassword);
-        jPanelRight.add(jpfPassword);
-        jPanelRight.add(jtfDBUsername);
-        jPanelRight.add(jbExit);
-        jPanelMain.add(jPanelRight);
-
+        JPanel jPanelMain = new JPanel(new GridLayout(4, 3, 15, 20));
+        jPanelMain.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 10 ));
+        jPanelMain.add(jlFirstname);
+        jPanelMain.add(jlLastname);
+        jPanelMain.add(jlPassword);
+        jPanelMain.add(jtfFirstname);
+        jPanelMain.add(jtfLastname);
+        jPanelMain.add(jpfPassword);
+        jPanelMain.add(new JLabel(""));
+        jPanelMain.add(jlDbUsername);
+        jPanelMain.add(jtfDBUsername);
+        jPanelMain.add(new JLabel(""));
+        jPanelMain.add(jbLogin);
+        jPanelMain.add(jbExit);
         this.add(BorderLayout.CENTER, jPanelMain);
-        this.add(BorderLayout.SOUTH, jtfStatus);
     }
 
     // Action Methods
@@ -115,36 +104,24 @@ public class Anmeldung extends JFrame implements ActionListener {
         this.getDbManager();
         String name = jtfFirstname.getText();
         String lastName = jtfLastname.getText();
-        Person person = new Person(name, lastName);
-        if(person.retrieveObject(dbManager)) {
-            Angestellter angestellter = new Angestellter(person);
-            angestellter.retrieveObject(dbManager);
-            jtfStatus.setText("Isim: " + angestellter.getPerson().getVorname() + " Abteilung: " + angestellter.getAbteilung());
+        if (name.equals("") && lastName.equals("")) {
+            System.out.println("Name and lastname is not provided.");
+            jtfStatus.setText("You should provide name and lastname");
         } else {
-            jtfStatus.setText("Mit diesen Angaben ist niemand gefunden.");
+            person = new Person(name, lastName);
+            if (person.retrieveObject(dbManager)) {
+                angestellter = new Angestellter(person);
+                angestellter.retrieveObject(dbManager);
+                jtfStatus.setText("Isim: " + angestellter.getPerson().getVorname() + " Abteilung: " + angestellter.getAbteilung());
+            } else {
+                jtfStatus.setText("Mit diesen Angaben ist niemand gefunden.");
+            }
         }
-//        try{
-//            String queryString = "SELECT * FROM personen WHERE vorname = '" + name + "' AND nachname = '" + lastName + "'";
-//            Statement stmt = connection.createStatement();
-//            ResultSet rs = stmt.executeQuery(queryString);
-//            while(rs.next()){
-//                String vorname = rs.getString("vorname");
-//                String nachname = rs.getString("nachname");
-//                jtfStatus.setText("Welcome " + vorname + " " + nachname);
-//                System.out.println("Works!");
-//            }
-//        }catch(SQLException sqle){
-//            jtfStatus.setText(sqle.getMessage());
-//            System.out.println(sqle.getMessage());
-//        }
-//        char[] password = jpfPassword.getPassword();
-//        String dbUser = jtfDBUsername.getText();
     }
 
     public void exitAction() {
         this.dispose();
         System.exit(0);
-        System.out.println("Exit Button clicked");
     }
 
     public void getDbManager() {
